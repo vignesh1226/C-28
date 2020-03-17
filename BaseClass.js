55bc5292af57134bb7675c6a7dfac6cac204955f
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-17 10:58:13 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -8,7 +8,7 @@ class BaseClass{
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage("sprites/base.png");
+        
         World.add(world, this.body);
       }
       display(){
@@ -16,8 +16,8 @@ class BaseClass{
         push();
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 0, 0, this.width, this.height);
+        rectMode(CENTER);
+        rect( 0, 0, this.width, this.height);
         pop();
       }
 }
